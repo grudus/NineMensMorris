2cbb69425bfcc220d88d9befd6ada2dc15900cdb
grudus <jakgruda@gmail.com>
2019-04-23 16:55:14 +0200

    Move canvas context to new class

---
+++ app/paint/GameCanvas.ts
@@ -0,0 +1,49 @@
+import { Point } from '../game/Point';
+
+export class GameCanvas {
+    public constructor(private ctx: CanvasRenderingContext2D, private squareSize: number) {}
+
+    public setColor(color: string) {
+        this.ctx.strokeStyle = color;
+        this.ctx.fillStyle = color;
+    }
+
+    public moveTo(point: Point) {
+        this.ctx.moveTo(
+            this.squareSize * point.colIndex + this.squareSize / 2,
+            this.squareSize * (point.row - 1) + this.squareSize / 2,
+        );
+    }
+
+    public lineTo(point: Point) {
+        this.ctx.lineTo(
+            this.squareSize * point.colIndex + this.squareSize / 2,
+            this.squareSize * (point.row - 1) + this.squareSize / 2,
+        );
+    }
+
+    public strokeRect(start: Point, end: Point) {
+        const x = this.squareSize * start.colIndex + this.squareSize / 2;
+        const y = this.squareSize * (start.row - 1) + this.squareSize / 2;
+
+        this.ctx.strokeRect(
+            x,
+            y,
+            this.squareSize * end.colIndex - x + this.squareSize / 2,
+            this.squareSize * (end.row - 1) - y + this.squareSize / 2,
+        );
+    }
+
+    public stroke() {
+        this.ctx.stroke();
+    }
+
+    public fillCircle(point: Point, radius: number) {
+        const xPosition = point.colIndex * this.squareSize + this.squareSize / 2;
+        const yPosition = (point.row - 1) * this.squareSize + this.squareSize / 2;
+
+        this.ctx.beginPath();
+        this.ctx.arc(xPosition, yPosition, radius, 0, 2 * Math.PI);
+        this.ctx.fill();
+    }
+}
--- app/paint/GameCanvasDrawer.ts
+++ app/paint/GameCanvasDrawer.ts
@@ -1,36 +1,38 @@
 import { NineMensMorrisGame } from '../game/NineMensMorrisGame';
 import { BoardPosition } from '../game/BoardPosition';
-import { point, Point } from '../game/Point';
+import { point } from '../game/Point';
+import { GameCanvas } from './GameCanvas';
 
 export class GameCanvasDrawer {
-    private squareSize: number;
+    private readonly squareSize: number;
     private readonly boardColor = '#212121';
+    private readonly gameCanvas: GameCanvas;
 
     public constructor(private canvas: HTMLCanvasElement, private game: NineMensMorrisGame) {
         this.fitToContainer(canvas);
+        this.squareSize = canvas.width / NineMensMorrisGame.BOARD_SIZE;
+        this.gameCanvas = new GameCanvas(canvas.getContext('2d'), this.squareSize);
+
         this.drawInitialCanvas(canvas);
     }
 
-    private drawInitialCanvas(canvas: HTMLCanvasElement) {
-        const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
-
-        this.squareSize = canvas.width / NineMensMorrisGame.BOARD_SIZE;
-        this.drawHelperLines(ctx, canvas);
-
-        this.drawDots(ctx);
-        this.drawLines(ctx);
-    }
-
-
     private fitToContainer(canvas: HTMLCanvasElement) {
-        // Make it visually fill the positioned parent
         canvas.style.width = '100%';
         canvas.style.height = '100%';
-        // ...then set the internal size to match
         canvas.width = canvas.offsetWidth;
         canvas.height = canvas.offsetHeight;
     }
-    private drawHelperLines(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+
+    private drawInitialCanvas(canvas: HTMLCanvasElement) {
+        this.drawHelperLines(canvas);
+
+        this.drawDots();
+        this.drawLines();
+    }
+
+    private drawHelperLines(canvas: HTMLCanvasElement) {
+        const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+
         for (let i = 0; i < NineMensMorrisGame.BOARD_SIZE; ++i) {
             ctx.moveTo(i * this.squareSize, 0);
             // noinspection JSSuspiciousNameCombination
@@ -42,69 +44,38 @@ export class GameCanvasDrawer {
             // noinspection JSSuspiciousNameCombination
             ctx.lineTo(canvas.height, i * this.squareSize);
         }
-        ctx.strokeStyle = '#e0e0e0';
+        this.gameCanvas.setColor('#e0e0e0');
         ctx.stroke();
     }
 
-    private drawDots(ctx: CanvasRenderingContext2D) {
-        ctx.moveTo(0, 0);
-        ctx.fillStyle = this.boardColor;
+    private drawDots() {
+        this.gameCanvas.setColor(this.boardColor);
+        const radius = 5;
 
         this.game.getBoard().forEach((board: BoardPosition) => {
-            const xPosition = board.point.colIndex * this.squareSize + this.squareSize / 2;
-            const yPosition = (board.point.row - 1) * this.squareSize + this.squareSize / 2;
-            const radius = 5;
-
-            ctx.beginPath();
-            ctx.arc(xPosition, yPosition, radius, 0, 2 * Math.PI);
-            ctx.fill();
+            this.gameCanvas.fillCircle(board.point, radius);
         });
     }
 
-    private drawLines(ctx: CanvasRenderingContext2D) {
-        ctx.strokeStyle = this.boardColor;
+    private drawLines() {
+        this.gameCanvas.setColor(this.boardColor);
 
-        this.strokeRect(ctx, point(1, 'a'), point(7, 'g'));
-        this.strokeRect(ctx, point(2, 'b'), point(6, 'f'));
-        this.strokeRect(ctx, point(3, 'c'), point(5, 'e'));
+        this.gameCanvas.strokeRect(point(1, 'a'), point(7, 'g'));
+        this.gameCanvas.strokeRect(point(2, 'b'), point(6, 'f'));
+        this.gameCanvas.strokeRect(point(3, 'c'), point(5, 'e'));
 
-        this.moveTo(ctx, point(1, 'd'));
-        this.lineTo(ctx, point(3, 'd'));
+        this.gameCanvas.moveTo(point(1, 'd'));
+        this.gameCanvas.lineTo(point(3, 'd'));
 
-        this.moveTo(ctx, point(5, 'd'));
-        this.lineTo(ctx, point(7, 'd'));
+        this.gameCanvas.moveTo(point(5, 'd'));
+        this.gameCanvas.lineTo(point(7, 'd'));
 
-        this.moveTo(ctx, point(4, 'a'));
-        this.lineTo(ctx, point(4, 'c'));
+        this.gameCanvas.moveTo(point(4, 'a'));
+        this.gameCanvas.lineTo(point(4, 'c'));
 
-        this.moveTo(ctx, point(4, 'e'));
-        this.lineTo(ctx, point(4, 'g'));
+        this.gameCanvas.moveTo(point(4, 'e'));
+        this.gameCanvas.lineTo(point(4, 'g'));
 
-        ctx.stroke();
-    }
-
-    private moveTo(ctx: CanvasRenderingContext2D, point: Point) {
-        ctx.moveTo(
-            this.squareSize * point.colIndex + this.squareSize / 2,
-            this.squareSize * (point.row - 1) + this.squareSize / 2,
-        );
-    }
-
-    private lineTo(ctx: CanvasRenderingContext2D, point: Point) {
-        ctx.lineTo(
-            this.squareSize * point.colIndex + this.squareSize / 2,
-            this.squareSize * (point.row - 1) + this.squareSize / 2,
-        );
-    }
-
-    private strokeRect(ctx: CanvasRenderingContext2D, start: Point, end: Point) {
-        let x = this.squareSize * start.colIndex + this.squareSize / 2;
-        let y = this.squareSize * (start.row - 1) + this.squareSize / 2;
-        ctx.strokeRect(
-            x,
-            y,
-            this.squareSize * end.colIndex - x + this.squareSize / 2,
-            this.squareSize * (end.row - 1) - y + this.squareSize / 2,
-        );
+        this.gameCanvas.stroke();
     }
 }
