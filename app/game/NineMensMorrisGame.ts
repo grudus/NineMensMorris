0831928176d1c45856fa002888456d8a83adf2fb
grudus <jakgruda@gmail.com>
2019-04-27 18:06:52 +0200

    Check for mill

--- app/game/NineMensMorrisGame.ts
+++ app/game/NineMensMorrisGame.ts
@@ -11,23 +11,48 @@ export class NineMensMorrisGame {
     private initialHandQueue: Player[];
     private currentPlayerMove = Player.PLAYER_1;
 
+    private cannotGoPoints = [
+        { from: { row: 4, col: 'c' }, to: { row: 4, col: 'e' } },
+        { from: { row: 3, col: 'd' }, to: { row: 5, col: 'd' } },
+    ];
+
     public constructor() {
         this.initialHandQueue = InitialGameHelper.initHandQueue();
         this.board = InitialGameHelper.initBoard();
     }
 
     public makeMove(point: Point) {
-        console.log(point);
-        console.log(this.possibleMoves(point));
         if (this.initialHandQueue.length) {
             const position = this.board.find(p => arePointsEqual(p.point, point));
             position.player = position.player === Player.NO_PLAYER ? this.currentPlayerMove : position.player;
             this.currentPlayerMove = this.initialHandQueue.pop();
         }
+        console.log('IS MILL?', this.isMill(point));
+    }
+
+    public isMill(changedPoint: Point): boolean {
+        const { colsInLine, rowsInLine } = this.findColsAndRowsInLine(changedPoint);
+
+        const checkMill = (inLineArray: BoardPosition[], point: Point): boolean => {
+            const inLineIndex = inLineArray.findIndex(p => arePointsEqual(p.point, point));
+            for (let i = 0; i < inLineArray.length; i += 3) {
+                if (inLineIndex >= i && inLineIndex < i + 3) {
+                    const currPlayer = inLineArray[inLineIndex].player;
+                    let millCount = 0;
+                    for (let j = 0; j < 3; j++) {
+                        if (currPlayer == inLineArray[i + j].player) millCount++;
+                    }
+                    if (millCount == 3) return true;
+                }
+            }
+            return false;
+        };
+
+        return checkMill(colsInLine, changedPoint) || checkMill(rowsInLine, changedPoint);
     }
 
     public canMakeMove(point: Point): boolean {
-        const triedPosition = this.board.find(p => arePointsEqual(p.point, point));
+        const triedPosition: BoardPosition = this.board.find(p => arePointsEqual(p.point, point));
         return triedPosition && triedPosition.player == Player.NO_PLAYER;
     }
 
@@ -43,16 +68,16 @@ export class NineMensMorrisGame {
         return neighbours;
     }
 
-    private findColsAndRowsInLine(point: Point): { colsInLine: Point[]; rowsInLine: Point[] } {
+    private findColsAndRowsInLine(point: Point): FindInLinePointsResults {
         return this.board.reduce(
             (acc, curr) => {
                 if (arePointsEqual(curr.point, point)) {
-                    acc.rowsInLine.push(point);
-                    acc.colsInLine.push(point);
+                    acc.rowsInLine.push(curr);
+                    acc.colsInLine.push(curr);
                 } else if (curr.point.colIndex === point.colIndex) {
-                    acc.colsInLine.push(curr.point);
+                    acc.colsInLine.push(curr);
                 } else if (curr.point.row === point.row) {
-                    acc.rowsInLine.push(curr.point);
+                    acc.rowsInLine.push(curr);
                 }
                 return acc;
             },
@@ -60,25 +85,22 @@ export class NineMensMorrisGame {
         );
     }
 
-    private findNearestPoints(point: Point, colsInLine: Point[], rowsInLine: Point[]) {
-        const sameColumnsIndex = colsInLine.indexOf(point);
-        const sameRowsIndex = rowsInLine.indexOf(point);
+    private findNearestPoints(point: Point, colsInLine: BoardPosition[], rowsInLine: BoardPosition[]): Point[] {
+        const sameColumnsIndex = colsInLine.findIndex(p => arePointsEqual(p.point, point));
+        const sameRowsIndex = rowsInLine.findIndex(p => arePointsEqual(p.point, point));
 
         return [
             colsInLine[sameColumnsIndex + 1],
             colsInLine[sameColumnsIndex - 1],
             rowsInLine[sameRowsIndex + 1],
             rowsInLine[sameRowsIndex - 1],
-        ].filter(x => x);
+        ]
+            .filter(x => x)
+            .map(p => p.point);
     }
 
     private filterNeighboursImpossibleToGo(point: Point, neighbours: Point[]) {
-        const cannotGoPoints = [
-            { from: { row: 4, col: 'c' }, to: { row: 4, col: 'e' } },
-            { from: { row: 3, col: 'd' }, to: { row: 5, col: 'd' } },
-        ];
-
-        cannotGoPoints.forEach(({ from, to }) => {
+        this.cannotGoPoints.forEach(({ from, to }) => {
             if (arePointsEqual(point, from)) {
                 const i = neighbours.findIndex(p => arePointsEqual(p, to));
                 neighbours.splice(i, 1);
@@ -88,4 +110,9 @@ export class NineMensMorrisGame {
             }
         });
     }
+}
+
+interface FindInLinePointsResults {
+    colsInLine: BoardPosition[];
+    rowsInLine: BoardPosition[];
 }
